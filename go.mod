56d56bc2f0a25e275ed46adaef2d0f8e4a8cadab
Ryss-D <70545185+Ryss-D@users.noreply.github.com>
2022-06-20 16:05:27 -0500

    fix: stable main file

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module main.go
+module telegram_bot
 
-go 1.16
+go 1.18
 
 require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
--- main.go
+++ main.go
@@ -2,10 +2,57 @@ package main
 
 import (
 	"log"
-
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+type doubt struct {
+	question    string
+	fullAnswer  string
+	shortAnswer string
+	link        string
+}
+
+var comoDepositar = doubt{
+	question:    "¿Cómo depositar",
+	fullAnswer:  "Tenemos muchos métodos de pago en línea o pago en efectivo muy cerca de ti ver nuestros métodos de recarga:\v Link video \v Link video",
+	shortAnswer: "Tenemos muchos métodos de pago \v Link video \v Link video",
+	link:        "link",
+}
+
+var comoApostar = doubt{
+	question:    "¿Cómo apostar",
+	fullAnswer:  "Acá te enseñamos como apostar mira el video",
+	shortAnswer: "Acá te enseñamos como apostar mira el video",
+	link:        "link",
+}
+
+var bonoDebut = doubt{
+	question:    "Bono Debut",
+	fullAnswer:  "El bono debut aplica para usuarios nuevos donde la primera recarga debe ser igual o mayor a $10.000.\vPara mayor información te invitamos a ingresar a los términos y condiciones en 👉🏼 https://www.wplay.co/mas/ayuda/200k-bono-debut-wplay/ 👍🏼",
+	shortAnswer: "Esta es la info sobre bono debut",
+	link:        "link",
+}
+
+var comoDescargar = doubt{
+	question:    "¿Cómo descargar",
+	fullAnswer:  "¿Cómo puedo descargar la App?\vPara descargar la App desde iOS ingresa aquí: https://apps.apple.com/co/app/id1490941885\vPara descargar la App desde Android ingresa aquí: https://play.google.com/store/apps/details?id=com.wplay.sports",
+	shortAnswer: "Como descargar",
+	link:        "link",
+}
+
+var tutoriales = doubt{
+	question:    "Tutoriales",
+	fullAnswer:  "Tutoriales",
+	shortAnswer: "Tutoriales",
+	link:        "link",
+}
+
+var comoRetirar = doubt{
+	question:    "¿Cómo retirar",
+	fullAnswer:  "Resumen de como retirar",
+	shortAnswer: "Resumen de como retirar",
+	link:        "link",
+}
 var numericInlineKeyboard = tgbotapi.NewInlineKeyboardMarkup(
 	tgbotapi.NewInlineKeyboardRow(
 		tgbotapi.NewInlineKeyboardButtonData(comoDepositar.question, comoDepositar.shortAnswer),
