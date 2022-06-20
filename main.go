89a7606eca81d053d6160c7de4259dc852669d04
Ryss-D <70545185+Ryss-D@users.noreply.github.com>
2022-06-20 15:12:36 -0500

    fix: back to stable declaration

--- main.go
+++ main.go
@@ -6,6 +6,42 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+var numericInlineKeyboard = tgbotapi.NewInlineKeyboardMarkup(
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData(comoDepositar.question, comoDepositar.shortAnswer),
+	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData(comoApostar.question, comoApostar.shortAnswer),
+	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData(bonoDebut.question, bonoDebut.shortAnswer),
+	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData(comoDescargar.question, comoDescargar.shortAnswer),
+	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData(tutoriales.question, tutoriales.shortAnswer),
+	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData(comoRetirar.question, comoRetirar.shortAnswer),
+	),
+)
+
+var numericKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton(comoDepositar.question),
+		tgbotapi.NewKeyboardButton(comoApostar.question),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton(bonoDebut.question),
+		tgbotapi.NewKeyboardButton(comoDescargar.question),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton(tutoriales.question),
+		tgbotapi.NewKeyboardButton(comoRetirar.question),
+	),
+)
+
 func main() {
 	bot, err := tgbotapi.NewBotAPI("5428271492:AAGCAeJPxZlJW6p6jv9htw8MIWZvHD4leHM")
 	if err != nil {
