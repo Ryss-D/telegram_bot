0ff2067b2ee40def9a85ac10d50478c3c0b3fcd7
Sebastian <70545185+Ryss-D@users.noreply.github.com>
2022-07-02 09:01:25 -0500

    feat: added extra references

---
+++ references/gcp_using_tgbotapi.go
@@ -0,0 +1,42 @@
+package gpt_compatible 
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func other() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	wh, _ := tgbotapi.NewWebhookWithCert("https://www.example.com:8443/"+bot.Token, "cert.pem")
+
+	_, err = bot.Request(wh)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	info, err := bot.GetWebhookInfo()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if info.LastErrorDate != 0 {
+		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
+	}
+
+	updates := bot.ListenForWebhook("/" + bot.Token)
+	go http.ListenAndServeTLS("0.0.0.0:8443", "cert.pem", "key.pem", nil)
+
+	for update := range updates {
+		log.Printf("%+v\n", update)
+	}
+}
